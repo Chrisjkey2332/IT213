6a308895fbc3de27a3308eaee3bc3b8448ff487a
Chrisjkey2332 <Key.christopher@yahoo.com>
2022-09-09 09:55:40 -0500

    Unit 3 fix

--- IT213_Key_Unit3.cs
+++ IT213_Key_Unit3.cs
@@ -19,12 +19,13 @@ namespace IT213_Key_Unit3
             //integer declarations
             gradeTotal = 0;
             gradeCount = 1;
-            
+
             //This wierd comment
             Console.WriteLine("**** Assignment 3: Section 1 - While Loop to Average Grades ****\n");
 
             //Loop part
-            while(gradeCount <= 10){
+            while (gradeCount <= 10)
+            {
                 Console.WriteLine("Enter another grade: ");
                 grade = Convert.ToInt32(Console.ReadLine());
                 gradeTotal = gradeTotal + grade;
@@ -46,9 +47,11 @@ namespace IT213_Key_Unit3
             Console.WriteLine("\n\n**** Assignment 3: Section 2 - Nested For Loops ****\n");
 
             //outer loop
-            for int(k = 5; k >= 1; k--){
+            for (int k = 5; k >= 1; k--)
+            {
                 //inner loop
-                for int(i = 0; i <= 10; i += 2){
+                for (int i = 0; i <= 10; i += 2)
+                {
                     Console.WriteLine("k = {0} i = {1}", k, i);
                 }
             }
@@ -56,24 +59,26 @@ namespace IT213_Key_Unit3
             //*********************************************************
             //****Assignment 3 Section 3
             //*********************************************************
-            
+
             //Set value for integers
-            int numberEntered;
-            int numberTotal;
+            int numberEntered = 0;
+            int numberTotal = 0;
 
             //Wierd comment again
             Console.WriteLine("\n\n**** Assignment 3: Section 3 - While Loop With Sentinel ****\n");
 
             //Keep prompting user to enter numbers
-            while(numberEntered != -1){
+            while (numberEntered != -1)
+            {
                 Console.WriteLine("Enter a positive number to be added to the total or -1 to end.");
                 numberEntered = Convert.ToInt32(Console.ReadLine());
-                if (numberEntered != -1) {
+                if (numberEntered != -1)
+                {
                     numberTotal = numberTotal + numberEntered;
                 }
             }
             Console.WriteLine("The sum of all numbers entered is " + numberTotal);
             Console.Read();
-        }   
+        }
     }
 }
